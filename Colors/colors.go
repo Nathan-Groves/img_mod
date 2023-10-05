eac3fff9f511c940452554a646e8ee5e7777eeda
Nathan Groves <104415198+Nathan-Groves@users.noreply.github.com>
2023-10-04 22:21:50 -0500

    Update colors.go

--- Colors/colors.go
+++ Colors/colors.go
@@ -1,1 +1,57 @@
+package Colors
 
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png" // import this package to decode PNGs
+	"os"
+)
+
+func printColors() {
+
+  var imageUrl string = ""
+
+  fmt.Println("Please type the image url you would like to use")
+
+  fmt.Scanln(imageUrl)
+  
+	reader, err := os.Open("imageUrl")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer reader.Close()
+
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+  
+	outfile, err := os.Create("printColors.txt")
+	if err != nil {
+		fmt.Println("Failed to create file:", err)
+		return
+	}
+	defer outfile.Close()
+
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			color := img.At(x, y)
+			r, g, b, _ := color.RGBA()
+      
+			colors := fmt.Sprintf("Pixel at (%d, %d) - R: %d, G: %d, B: %d\n", x, y, r>>8, g>>8, b>>8)
+			outfile.WriteString(colors)
+
+			colors = ""
+		}
+	}
+  	fmt.Println("Saving pixel output to 'printColors.txt'" + "...done")
+
+}
